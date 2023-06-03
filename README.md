08e0c60297847f37c66ce75e0b11a1e3ec500484
victor <victoroyiga01@gmail.com>
2023-06-03 04:33:41 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Building and Deploying a Modern YouTube Clone Application with React JS and Material UI 5
+
+
+### Showcase your dev skills with https://jsmastery.pro/youtubepractical experience and land the coding career of your dreams
+💻 victor - 
+✅ 
+
+📙
---
+++ src/utils/fetchFromAPI.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+export const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
+
+const options = {
+  params: {
+    maxResults: 50,
+  },
+  headers: {
+    'X-RapidAPI-Key': 'ba32d6a5c4mshe5af5aaf67fca57p19943fjsne32cefbeff33',
+    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
+  },
+};
+
+export const fetchFromAPI = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+
+  return data;
+};
